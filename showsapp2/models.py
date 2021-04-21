16c6325c8c7e158b062e87b19d12ad66cec4ef91
Ciku <flogert.ciku@vodafone.com>
2021-04-21 22:28:20 +0200

    validations

--- showsapp2/models.py
+++ showsapp2/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 5:
+            errors["title"] = "Blog name should be at least 5 characters"
+        if len(postData['network']) < 5:
+            errors["network"] = "Blog name should be at least 5 characters"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "Blog description should be at least 10 characters"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=45)
@@ -9,3 +19,5 @@ class Show(models.Model):
     release_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+
--- showsapp2/views.py
+++ showsapp2/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse , redirect
+from django.contrib import messages
 from showsapp2.models import *
 
  #route /shows   GET
@@ -13,37 +14,54 @@ def new(request):
 
 #route /shows/create    POST
 def create(request):
-    print(request.POST)
-    show = Show.objects.create(title=request.POST['title'],
-        network=request.POST['network'],
-        release_date=request.POST['release_date'],
-        desc=request.POST['desc'])
-    return redirect("/shows/"+str(show.id))
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect("/shows/new")
+    else:
+        show = Show.objects.create(title=request.POST['title'],
+            network=request.POST['network'],
+            release_date=request.POST['release_date'],
+            desc=request.POST['desc'])
+        return redirect("/shows/"+str(show.id))
 
 #route /shows/<str:id>  GET
 
 def show(request,id):
     context = {
-    	"show": Show.objects.get(id=id)
-    }        
+            "show": Show.objects.get(id=id)
+                }        
     return render(request, "show.html", context)
 
 #route /shows/<str:id>/edit     GET
+
 def edit(request,id):
     context = {
-    	"show": Show.objects.get(id=id)
-    }        
-    return render(request, "edit.html", context)
+        "show2" : Show.objects.get(id=id)
+    }      
+    return render(request,"edit.html", context)
 
 #route /shows/<str:id>     GET
 def update(request,id):
-    show = Show.objects.get(id=id)
-    show.title=request.POST['title']
-    show.network=request.POST['network']
-    show.release_date=request.POST['release_date']
-    show.desc=request.POST['desc']
-    show.save()
-    return redirect("/shows/"+str(show.id))
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect("/shows/"+ str(id)+"/edit")
+    else :
+        show = Show.objects.get(id=id)
+        show.title=request.POST['title']
+        show.network=request.POST['network']
+        show.release_date=request.POST['release_date']
+        show.desc=request.POST['desc']
+        show.save()
+        messages.success(request, "Show successfully updated")
+        return redirect("/shows/"+str(show.id))
 
 #route /shows/<str:id>/delete    GET
 def destroy(request,id):
